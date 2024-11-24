54c712547b28c6437f809970fe1a02110d2f5748
Julien Castellano <julien.crm@gmail.com>
2024-11-24 23:57:44 +0100

    Update projet2.py

--- wcs/projet_2/projet2.py
+++ wcs/projet_2/projet2.py
@@ -1,1 +1,12 @@
+import streamlit as st
+
+def afficher_wcs_projet2(): 
+  st.write("## Projet 2 : Machine Learning 🧠")
+  st.write("""
+      - **Objectif** : Développer un modèle de Machine Learning pour des prédictions précises.
+      - **Étapes** :
+        1. Préparation des données (nettoyage et analyse).
+        2. Entraînement avec différents modèles (supervisés et non supervisés).
+        3. Évaluation des performances et visualisation des résultats.
+      """)
 
