a02f162a5c769d30043bf29f017cd8bc271a965f
Julien Castellano <julien.crm@gmail.com>
2024-11-25 00:15:56 +0100

    Update projet1.py

--- wcs/projet_1/projet1.py
+++ wcs/projet_1/projet1.py
@@ -193,22 +193,22 @@ def afficher_wcs_projet1():
         dict(
             title="",
             text="",
-            img="wcs/projet_1/ressources/page 1.png"
+            img="wcs/projet_1/ressources/page1.png"
             ),
         dict(
             title="",
             text="",
-        img="wcs/projet_1/ressources/page 2.png"
+        img="wcs/projet_1/ressources/page2.png"
         ),
         dict(
             title="",
             text="",
-            img="wcs/projet_1/ressources/page 3.png"
+            img="wcs/projet_1/ressources/page3.png"
             ),
         dict(
             title="",
             text="",
-            img="wcs/projet_1/ressources/page 4.png"
+            img="wcs/projet_1/ressources/page4.png"
         ),
 
         ]
