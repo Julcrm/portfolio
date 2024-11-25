7835979d4abdd1c36f46ec78a1786b5a2207b12f
Julien Castellano <julien.crm@gmail.com>
2024-11-25 09:28:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from wcs_projet1 import afficher_wcs_projet1
-from wcs_projet2 import afficher_wcs_projet2
+from wcs.projet_1.projet1 import afficher_wcs_projet1
+from wcs.projet_2.projet2 import afficher_wcs_projet2
 
 # Configuration de la page
 st.set_page_config(
